7eb9a5f60cf3f3d6bd0de1ad4073ffac1f9f4052
bia-pequeno <bia.2017pequeno@gmail.com>
2022-01-22 19:35:50 -0300

    concluido

---
+++ script.js
@@ -0,0 +1,25 @@
+function Hi() {
+    console.log("Hello Mate")
+}
+Hi()
+
+function Me(nome) {
+    console.log(`Olá ${nome}, seja bem vinda!`)
+}
+Me("Beatriz");
+
+function box(nome, numero, estilomusical) {
+    console.log(`${nome}, ${numero}, ${estilomusical}`)
+}
+box("Beatriz","17","pop")
+
+function fav(filme, musica) {
+    console.log(`${filme}, ${musica}`)
+}
+fav("Spider-man No Way Home","moment-Vierre Cloud")
+
+function math(num) {
+    return num*3
+}
+
+console.log(math(18))
